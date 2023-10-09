d6284845e032efc0188356559af215922e1acb60
Miguel Herreros Pinilla <mhpcalatrava@gmail.com>
2023-10-09 18:58:53 +0200

    Commit segundo

--- app/src/main/java/com/example/variosactivitiescondatos/MainActivity.kt
+++ app/src/main/java/com/example/variosactivitiescondatos/MainActivity.kt
@@ -23,6 +23,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun irAVentana2() {
         var miIntent: Intent = Intent(this, Ventana2::class.java)//Comunicamos con Ventana2
+        miIntent.putExtra("nombre", binding.cajaNombre.text.toString())
+        miIntent.putExtra("edad", binding.cajaEdad.text.toString())
         startActivity(miIntent)
     }
 }
--- app/src/main/java/com/example/variosactivitiescondatos/Ventana2.kt
+++ app/src/main/java/com/example/variosactivitiescondatos/Ventana2.kt
@@ -2,10 +2,29 @@ package com.example.variosactivitiescondatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.variosactivitiescondatos.databinding.ActivityMainBinding
+import com.example.variosactivitiescondatos.databinding.ActivityVentana2Binding
 
 class Ventana2 : AppCompatActivity() {
+
+    lateinit var binding : ActivityVentana2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ventana2)
+
+        binding = ActivityVentana2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var nombre = intent.getStringExtra("nombre")
+        var edad = intent.getStringExtra("edad")
+
+        binding.cajaNombre.setText(nombre)
+        binding.cajaEdad.setText(edad)
+
+        binding.boton.setOnClickListener {
+            finish()
+        }
+
     }
 }
