e97ce5975e7abf3c654dfe8a3aa5bc9af049575d
Miguel Herreros Pinilla <mhpcalatrava@gmail.com>
2023-10-09 18:37:34 +0200

    Commit segundo

---
+++ app/src/main/java/com/example/variosactivitiescondatos/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.variosactivitiescondatos
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.example.variosactivitiescondatos.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding//Declaramos nuestro binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)//Asignamos el binding
+        setContentView(binding.root)//¿?
+        binding.boton.setOnClickListener {
+            irAVentana2()
+        }
+
+
+    }
+
+    private fun irAVentana2() {
+        var miIntent: Intent = Intent(this, Ventana2::class.java)//Comunicamos con Ventana2
+        startActivity(miIntent)
+    }
+}
